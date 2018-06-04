4e3ee4a4c5240100919dd58030a3c687188f59cd
Andrew <andrew.bestbier@gmail.com>
2018-06-04 17:15:34 +0100

    Convert partnerEngineerEmail to suppliedEmail

--- ios/RNSalesforceChat.m
+++ ios/RNSalesforceChat.m
@@ -11,11 +11,11 @@ SCSChatConfiguration *chatConfiguration;
 
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(configLaunch:(NSString *)subject origin:(NSString *)origin currencyISOCode: (NSString *)currencyISOCode status: (NSString *)status contactType:(NSString *)contactType locale:(NSString *)locale partnerEngineerEmail:(NSString *)partnerEngineerEmail partnerEngineer:(NSString *)partnerEngineer suppliedName:(NSString *)suppliedName)
+RCT_EXPORT_METHOD(configLaunch:(NSString *)subject origin:(NSString *)origin currencyISOCode: (NSString *)currencyISOCode status: (NSString *)status contactType:(NSString *)contactType locale:(NSString *)locale suppliedEmail:(NSString *)suppliedEmail email:(NSString *)email suppliedName:(NSString *)suppliedName)
 {
     prechatFields = [NSMutableArray array];
     prechatEntities = [NSMutableArray array];
-    
+
     // Prechat fields
     SCSPrechatTextInputObject* subjectField = [[SCSPrechatTextInputObject alloc]
                                                initWithLabel:NSLocalizedString(@"Subject", nil)];
@@ -37,16 +37,16 @@ RCT_EXPORT_METHOD(configLaunch:(NSString *)subject origin:(NSString *)origin cur
     SCSPrechatObject* localeField = [[SCSPrechatObject alloc]
                                      initWithLabel:@"Locale__c" value: locale];
     [prechatFields addObject:localeField];
-    SCSPrechatObject* partnerEngineerEmailField = [[SCSPrechatObject alloc]
-                                                   initWithLabel:@"PartnerEngineerEmail__c" value: partnerEngineerEmail];
-    [prechatFields addObject:partnerEngineerEmailField];
+    SCSPrechatObject* suppliedEmailField = [[SCSPrechatObject alloc]
+                                                   initWithLabel:@"SuppliedEmail" value: suppliedEmail];
+    [prechatFields addObject:suppliedEmailField];
     SCSPrechatObject* suppliedNameField = [[SCSPrechatObject alloc]
                                            initWithLabel:@"SuppliedName" value: suppliedName];
     [prechatFields addObject:suppliedNameField];
-    SCSPrechatObject* partnerEngineerField = [[SCSPrechatObject alloc]
-                                              initWithLabel:@"PartnerEngineerRef__c" value: partnerEngineer];
-    [prechatFields addObject:partnerEngineerField];
-    
+    SCSPrechatObject* emailField = [[SCSPrechatObject alloc]
+                                              initWithLabel:@"Email" value: email];
+    [prechatFields addObject:emailField];
+
     // Prechat entities
     SCSPrechatEntityField* subjectEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"Subject" label:@"Subject"];
     SCSPrechatEntity* subjectEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"Subject"];
@@ -54,63 +54,63 @@ RCT_EXPORT_METHOD(configLaunch:(NSString *)subject origin:(NSString *)origin cur
     [prechatEntities addObject: subjectEntity];
     subjectEntity.saveToTranscript = @"Case";
     subjectEntity.linkToEntityField = @"Subject";
-    
+
     SCSPrechatEntityField* originEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"Origin" label:@"Origin"];
     SCSPrechatEntity* originEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"Origin"];
     [originEntity.entityFieldsMaps addObject:originEntityField];
     [prechatEntities addObject: originEntity];
     originEntity.saveToTranscript = @"Case";
     originEntity.linkToEntityField = @"Origin";
-    
-    
+
+
     SCSPrechatEntityField* currencyISOCodeEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"CurrencyISOCode" label:@"CurrencyISOCode"];
     SCSPrechatEntity* currencyISOCodeEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"CurrencyISOCode"];
     [currencyISOCodeEntity.entityFieldsMaps addObject:currencyISOCodeEntityField];
     [prechatEntities addObject: currencyISOCodeEntity];
     currencyISOCodeEntity.saveToTranscript = @"Case";
     currencyISOCodeEntity.linkToEntityField = @"CurrencyISOCode";
-    
+
     SCSPrechatEntityField* statusEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"Status" label:@"Status"];
     SCSPrechatEntity* statusEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"Status"];
     [statusEntity.entityFieldsMaps addObject:statusEntityField];
     [prechatEntities addObject: statusEntity];
     statusEntity.saveToTranscript = @"Case";
     statusEntity.linkToEntityField = @"Status";
-    
+
     SCSPrechatEntityField* contactTypeEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"ContactType__c" label:@"ContactType__c"];
     SCSPrechatEntity* contactTypeEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"ContactType__c"];
     [contactTypeEntity.entityFieldsMaps addObject:contactTypeEntityField];
     [prechatEntities addObject: contactTypeEntity];
     contactTypeEntity.saveToTranscript = @"Case";
     contactTypeEntity.linkToEntityField = @"ContactType__c";
-    
+
     SCSPrechatEntityField* localeEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"Locale__c" label:@"Locale__c"];
     SCSPrechatEntity* localeEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"Locale__c"];
     [localeEntity.entityFieldsMaps addObject:localeEntityField];
     [prechatEntities addObject: localeEntity];
     localeEntity.saveToTranscript = @"Case";
     localeEntity.linkToEntityField = @"Locale__c";
-    
-    SCSPrechatEntityField* partnerEngineerEmailEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"PartnerEngineerEmail__c" label:@"PartnerEngineerEmail__c"];
-    SCSPrechatEntity* partnerEngineerEmailEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"PartnerEngineerEmail__c"];
-    [partnerEngineerEmailEntity.entityFieldsMaps addObject:partnerEngineerEmailEntityField];
-    [prechatEntities addObject: partnerEngineerEmailEntity];
-    partnerEngineerEmailEntity.saveToTranscript = @"Case";
-    partnerEngineerEmailEntity.linkToEntityField = @"PartnerEngineerEmail__c";
-    
+
+    SCSPrechatEntityField* suppliedEmailEntityField = [[SCSPrechatEntityField alloc] initWithFieldName: @"SuppliedEmail" label:@"SuppliedEmail"];
+    SCSPrechatEntity* suppliedEmailEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"SuppliedEmail"];
+    [suppliedEmailEntity.entityFieldsMaps addObject:suppliedEmailEntityField];
+    [prechatEntities addObject: suppliedEmailEntity];
+    suppliedEmailEntity.saveToTranscript = @"Case";
+    suppliedEmailEntity.linkToEntityField = @"SuppliedEmail";
+
     SCSPrechatEntityField* suppliedNameEntityField = [[SCSPrechatEntityField alloc] initWithFieldName: @"SuppliedName" label:@"SuppliedName"];
     SCSPrechatEntity* suppliedNameEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"SuppliedName"];
     [suppliedNameEntity.entityFieldsMaps addObject:suppliedNameEntityField];
     [prechatEntities addObject: suppliedNameEntity];
     suppliedNameEntity.saveToTranscript = @"Case";
     suppliedNameEntity.linkToEntityField = @"SuppliedName";
-    
-    SCSPrechatEntityField* partnerEngineerEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"PartnerEngineerRef__c" label:@"PartnerEngineerRef__c"];
-    SCSPrechatEntity* partnerEngineerEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"PartnerEngineerRef__c"];
-    [partnerEngineerEntity.entityFieldsMaps addObject:partnerEngineerEntityField];
-    partnerEngineerEntity.saveToTranscript = @"Case";
-    partnerEngineerEntity.linkToEntityField = @"PartnerEngineerRef__c";
-    [prechatEntities addObject: partnerEngineerEntity];
+
+    SCSPrechatEntityField* emailEntityField = [[SCSPrechatEntityField alloc] initWithFieldName:@"Email" label:@"Email"];
+    SCSPrechatEntity* emailEntity = [[SCSPrechatEntity alloc] initWithEntityName: @"Email"];
+    [emailEntity.entityFieldsMaps addObject:emailEntityField];
+    emailEntity.saveToTranscript = @"Case";
+    emailEntity.linkToEntityField = @"Email";
+    [prechatEntities addObject: emailEntity];
 }
 
 RCT_EXPORT_METHOD(configChat:(NSString *)orgId deploymentId:(NSString *)deploymentId buttonId: (NSString *) buttonId: (NSString *) liveAgentPod)
@@ -120,10 +120,10 @@ RCT_EXPORT_METHOD(configChat:(NSString *)orgId deploymentId:(NSString *)deployme
                                                  orgId: orgId
                                           deploymentId: deploymentId
                                               buttonId: buttonId];
-    
+
     [chatConfiguration.prechatFields addObjectsFromArray:prechatFields];
     [chatConfiguration.prechatEntities addObjectsFromArray: prechatEntities];
-    
+
 }
 
 RCT_EXPORT_METHOD(launch:(RCTResponseSenderBlock)callback)
@@ -131,7 +131,7 @@ RCT_EXPORT_METHOD(launch:(RCTResponseSenderBlock)callback)
     [[SCServiceCloud sharedInstance].chat
      determineAvailabilityWithConfiguration:chatConfiguration
      completion:^(NSError *error, BOOL available) {
-         
+
          if (error != nil) {
              // Handle error
          }
@@ -146,4 +146,3 @@ RCT_EXPORT_METHOD(launch:(RCTResponseSenderBlock)callback)
 }
 
 @end
-
